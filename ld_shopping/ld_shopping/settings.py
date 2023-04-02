6914324135c66f8afd41867c6c4ca4615fe345f4
8argoN <jsrop07@gmail.com>
2023-04-03 01:45:07 +0900

    230403_2

--- ld_shopping/ld_shopping/settings.py
+++ ld_shopping/ld_shopping/settings.py
@@ -1,5 +1,6 @@
 
-import os
+import os,json
+from django.core.exceptions import ImproperlyConfigured
 """
 Django settings for ld_shopping project.
 
@@ -22,7 +23,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*t6xeh&f%z@&#9sm0m^0h!++j)@dc23bv@7mw#@==qr90mge!8'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+# secrets.json 파일에서 SECRET_KEY 가져오기    
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- shoppingMall/reviews/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import product_list_view, product_detail_view, review_create_view
-
-urlpatterns = [
-    path('', product_list_view, name='product_list'),
-    path('<int:1>/', product_detail_view, name='product_detail'),
-    path('<int:pk>/review/', review_create_view, name='review_create'),
-]
